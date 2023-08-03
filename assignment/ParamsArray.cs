894da67dcc270c7c18ef5a8546229e7da8b29092
Ved Prakash <91253410+vp171212@users.noreply.github.com>
2023-08-03 16:33:41 +0530

    Add files via upload

---
+++ assignment/ParamsArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paramsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = sumUp(10, 26, 23, 75);
+            int sum = sumUp(138, 456, 65, 78, 23, 45);
+            Console.WriteLine("Summation is: "+total);
+            Console.WriteLine("Total sum is: "+sum);
+        }
+        static int sumUp(params int[] amount)
+        {
+            int summation = 0;
+            foreach(int numbers in amount) 
+            {
+                summation += numbers;
+            }
+            return summation;
+        }
+    }
+}
---
+++ assignment/TryParse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryParseDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "sfg";
+            int intStr; bool intResultTryParse = int.TryParse(str, out intStr);
+            if (intResultTryParse == true)
+            {
+                Console.WriteLine(intStr);
+            }
+            else
+            {
+                Console.WriteLine("Input is not in integer format");
+            }
+            Console.WriteLine("Enter a number");
+            string str1=Console.ReadLine();
+            int intStr1;
+                bool result=int.TryParse(str1, out intStr1);
+            if (result == true)
+            Console.WriteLine("input is: "+intStr1);
+            else
+            { Console.WriteLine("Please enter a valid Integer"); }
+        }
+    }
+}
